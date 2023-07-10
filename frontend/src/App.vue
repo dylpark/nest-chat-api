285101a2fc27c5076de95aff61b4a07861a98ad9
Dylan Park <64296934+dylpark@users.noreply.github.com>
2023-07-10 22:18:00 +1000

    Remove boiler plate and configure tailwind

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,19 +1,14 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
+  <header></header>
   <main>
-    <TheWelcome />
+    <div class="bg-blue-500">
+      <div class="text-white">It's over Anakin, <br />I have the high ground.</div>
+    </div>
+    <div class="bg-gray-200">
+      <div class="text-white">You underestimate my power!</div>
+    </div>
   </main>
 </template>
 
