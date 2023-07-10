a0e4b507954443c1c5512eb2bd8f5cd9fe425724
Dylan Park <64296934+dylpark@users.noreply.github.com>
2023-07-10 21:56:57 +1000

    Adding frontend

--- backend/src/pusher/pusher.service.ts
+++ backend/src/pusher/pusher.service.ts
@@ -7,10 +7,10 @@ export class PusherService {
 
   constructor() {
     this.pusher = new Pusher({
-      appId: '',
-      key: '',
-      secret: '',
-      cluster: '',
+      appId: '1632329',
+      key: '4318d6f49f4a824002c6',
+      secret: 'd3de1056ada96109db64',
+      cluster: 'ap4',
       useTLS: true,
     });
   }
