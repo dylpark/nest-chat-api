b6623df6850e4aa81887cddd81f47dd120d03d86
Dylan Park <64296934+dylpark@users.noreply.github.com>
2023-07-11 17:42:41 +1000

    Regenerating exposed secrets and creating .env

--- backend/src/pusher/pusher.service.ts
+++ backend/src/pusher/pusher.service.ts
@@ -7,9 +7,9 @@ export class PusherService {
 
   constructor() {
     this.pusher = new Pusher({
-      appId: '1632329',
-      key: '4318d6f49f4a824002c6',
-      secret: 'd3de1056ada96109db64',
+      appId: process.env.APP_KEY,
+      key: process.env.PUSHER_KEY,
+      secret: process.env.PUSHER_SECRET,
       cluster: 'ap4',
       useTLS: true,
     });
