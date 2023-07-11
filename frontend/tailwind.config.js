3ec678c961ce056aba48a073d553ac30de1c72af
Dylan Park <64296934+dylpark@users.noreply.github.com>
2023-07-11 17:42:00 +1000

    Updating tailwind config and gitignore

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 // eslint-disable-next-line no-undef
 module.exports = {
-  content: [],
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {}
   },
